539cde22b36d27a31a1c2a3f89a5a6183cf9728f
Robert Daniels <robdaniels2001@gmail.com>
2025-01-26 17:30:31 -0500

    Define component parameters as constants

--- Chip8.h
+++ Chip8.h
@@ -2,18 +2,26 @@
 
 #include <cstdint>
 
+const unsigned int REGISTER_COUNT = 16;
+const unsigned int MEMORY_SIZE = 4096;
+const unsigned int STACK_SIZE = 16;
+const unsigned int KEY_COUNT = 16;
+const unsigned int VIDEO_WIDTH = 64;
+const unsigned int VIDEO_HEIGHT = 32;
 
+// Chip-8 emulator class
 class Chip8
 {
-public:
-	uint8_t registers[16]{}; // V0 to VF
-	uint8_t memory[4096]{}; // 4KB memory
+private:
+	uint8_t registers[REGISTER_COUNT]{}; // V0 to VF
+	uint8_t memory[MEMORY_SIZE]{}; // 4KB memory
 	uint16_t index{}; // Index register
 	uint16_t pc{}; // Program counter
-	uint16_t stack[16]{}; // Stack for function calls
+	uint16_t stack[STACK_SIZE]{}; // Stack for function calls
 	uint8_t sp{}; // Stack pointer
 	uint8_t delayTimer{}; // Delay timer
 	uint8_t soundTimer{}; // Sound timer
-	uint8_t keypad[16]{}; // Hex keypad
-	uint32_t video[64 * 32]{}; // 64x32 monochrome display
+	uint8_t keypad[KEY_COUNT]{}; // Hex keypad
+	uint32_t video[VIDEO_WIDTH * VIDEO_HEIGHT]{}; // 64x32 monochrome display
+
 };
