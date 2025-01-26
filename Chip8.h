fe1ae16e4f332f061c76fad382b69803a273bd1c
Robert Daniels <robdaniels2001@gmail.com>
2025-01-26 17:21:19 -0500

    Define Chip 8 components in class

--- Chip8.h
+++ Chip8.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include <cstdint>
+
+
+class Chip8
+{
+public:
+	uint8_t registers[16]{}; // V0 to VF
+	uint8_t memory[4096]{}; // 4KB memory
+	uint16_t index{}; // Index register
+	uint16_t pc{}; // Program counter
+	uint16_t stack[16]{}; // Stack for function calls
+	uint8_t sp{}; // Stack pointer
+	uint8_t delayTimer{}; // Delay timer
+	uint8_t soundTimer{}; // Sound timer
+	uint8_t keypad[16]{}; // Hex keypad
+	uint32_t video[64 * 32]{}; // 64x32 monochrome display
+};
