0238a59b1f05ceb86135f81c6134ff1af2df70dd
Robert Daniels <robdaniels2001@gmail.com>
2025-01-30 20:21:51 -0500

    Implement main execution loop

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,49 @@
+#include <sstream>
+#include <iostream>
+#include <chrono>
+#include "Chip8.h"
+#include "Platform.h"
 
-
-int main()
+int main(int argc, char** argv)
 {
+	if (argc != 4)
+	{
+		std::cerr << "Usage: " << argv[0] << " <Scale> <Delay> <ROM>\n";
+		std::exit(EXIT_FAILURE);
+	}
+
+	int videoScale;
+	int cycleDelay;
+	std::istringstream(argv[1]) >> videoScale;
+	std::istringstream(argv[2]) >> cycleDelay;
+	char const* romFilename = argv[3];
+
+	Platform platform("CHIP-8 Emulator", VIDEO_WIDTH * videoScale, VIDEO_HEIGHT * videoScale, VIDEO_WIDTH, VIDEO_HEIGHT);
+
+	Chip8 chip8;
+	chip8.LoadROM(romFilename);
+
+	int videoPitch = sizeof(chip8.video[0]) * VIDEO_WIDTH;
+
+	auto lastCycleTime = std::chrono::high_resolution_clock::now();
+	bool quit = false;
+
+	while (!quit)
+	{
+		quit = platform.ProcessInput(chip8.keypad);
+
+		auto currentTime = std::chrono::high_resolution_clock::now();
+		float dt = std::chrono::duration<float, std::chrono::milliseconds::period>(currentTime - lastCycleTime).count();
+
+		if (dt > cycleDelay)
+		{
+			lastCycleTime = currentTime;
+
+			chip8.Cycle();
+
+			platform.Update(chip8.video, videoPitch);
+		}
+	}
 
 	return 0;
 }
