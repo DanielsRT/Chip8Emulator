e8ec383d34d44791932b7c1f14d8e00a1518c631
Robert Daniels <robdaniels2001@gmail.com>
2025-01-31 22:30:05 -0500

    Move Platform functions to main

--- Main.cpp
+++ Main.cpp
@@ -1,49 +1,127 @@
-#include <sstream>
 #include <iostream>
 #include <chrono>
-#include "Chip8.h"
-#include "Platform.h"
+#include <thread>
+#include "stdint.h"
+#include "SDL3/SDL.h"
 
-int main(int argc, char** argv)
-{
-	if (argc != 4)
-	{
-		std::cerr << "Usage: " << argv[0] << " <Scale> <Delay> <ROM>\n";
-		std::exit(EXIT_FAILURE);
-	}
 
-	int videoScale;
-	int cycleDelay;
-	std::istringstream(argv[1]) >> videoScale;
-	std::istringstream(argv[2]) >> cycleDelay;
-	char const* romFilename = argv[3];
+#include "chip8.h"
 
-	Platform platform("CHIP-8 Emulator", VIDEO_WIDTH * videoScale, VIDEO_HEIGHT * videoScale, VIDEO_WIDTH, VIDEO_HEIGHT);
+using namespace std;
 
-	Chip8 chip8;
-	chip8.LoadROM(romFilename);
+// Keypad keymap
+uint8_t keymap[16] = {
+    SDLK_X,
+    SDLK_1,
+    SDLK_2,
+    SDLK_3,
+    SDLK_Q,
+    SDLK_W,
+    SDLK_E,
+    SDLK_A,
+    SDLK_S,
+    SDLK_D,
+    SDLK_Z,
+    SDLK_C,
+    SDLK_4,
+    SDLK_R,
+    SDLK_F,
+    SDLK_V,
+};
 
-	int videoPitch = sizeof(chip8.video[0]) * VIDEO_WIDTH;
+int main(int argc, char** argv) {
 
-	auto lastCycleTime = std::chrono::high_resolution_clock::now();
-	bool quit = false;
+    // Command usage
+    /*if (argc != 2) {
+        cout << "Usage: chip8 <ROM file>" << endl;
+        return 1;
+    }*/
 
-	while (!quit)
-	{
-		quit = platform.ProcessInput(chip8.keypad);
+    Chip8 chip8 = Chip8();
 
-		auto currentTime = std::chrono::high_resolution_clock::now();
-		float dt = std::chrono::duration<float, std::chrono::milliseconds::period>(currentTime - lastCycleTime).count();
+    int windowWidth = 1024;
+    int windowHeight = 512;
 
-		if (dt > cycleDelay)
-		{
-			lastCycleTime = currentTime;
+    SDL_Window* window;
 
-			chip8.Cycle();
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        exit(1);
+    }
+    window = SDL_CreateWindow(
+        "CHIP-8 Emulator",
+		windowWidth, windowHeight, SDL_WINDOW_RESIZABLE
+    );
+    if (window == NULL) {
+        printf("Window could not be created! SDL_Error: %s\n",
+            SDL_GetError());
+        exit(2);
+    }
 
-			platform.Update(chip8.video, videoPitch);
-		}
-	}
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+    SDL_SetRenderLogicalPresentation(renderer, windowWidth, windowHeight, SDL_LOGICAL_PRESENTATION_LETTERBOX);
 
-	return 0;
+    // Texture that stores frame buffer
+    SDL_Texture* sdlTexture = SDL_CreateTexture(renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        64, 32);
+
+    // Temporary pixel buffer
+    uint32_t pixels[2048];
+
+
+    if (!chip8.LoadROM("PONG"))
+        return 2;
+
+    while (true) {
+        chip8.Cycle();
+
+        // Process SDL events
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) exit(0);
+
+            // Process keydown events
+            if (event.type == SDL_EVENT_KEY_DOWN) {
+                if (event.key.key == SDLK_ESCAPE)
+                    exit(0);
+
+                for (int i = 0; i < 16; ++i) {
+                    if (event.key.key == keymap[i]) {
+                        chip8.keypad[i] = 1;
+                    }
+                }
+            }
+            // Process keyup events
+            if (event.type == SDL_EVENT_KEY_UP) {
+                for (int i = 0; i < 16; ++i) {
+                    if (event.key.key == keymap[i]) {
+                        chip8.keypad[i] = 0;
+                    }
+                }
+            }
+        }
+
+        // If draw occurred, redraw SDL screen
+        if (chip8.drawFlag) {
+            chip8.drawFlag = false;
+
+            // Store pixels in temporary buffer
+            for (int i = 0; i < 2048; ++i) {
+                uint8_t pixel = chip8.video[i];
+                pixels[i] = (0x00FFFFFF * pixel) | 0xFF000000;
+            }
+            // Update SDL texture
+            SDL_UpdateTexture(sdlTexture, NULL, pixels, 64 * sizeof(Uint32));
+            // Clear screen and render
+            SDL_RenderClear(renderer);
+            SDL_RenderTexture(renderer, sdlTexture, NULL, NULL);
+            SDL_RenderPresent(renderer);
+        }
+
+        // Sleep to slow down emulation speed
+        std::this_thread::sleep_for(std::chrono::microseconds(1000));
+
+    }
 }
